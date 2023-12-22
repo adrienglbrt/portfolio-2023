9b7e0817ce7d010d596e55ff89cc824240431e41
Adrien <adrien.galibert@gmail.com>
2023-12-22 11:48:55 +0100

    Update next / previous project links

--- src/pages/projects/[slug].js
+++ src/pages/projects/[slug].js
@@ -1,8 +1,9 @@
 import { useRouter } from 'next/router'
 import projectsData from '@/data/projects.json'
-import ButtonInternal from '@/components/buttonInternal'
+import Button from '@/components/button'
 import Image from 'next/image'
 import Wrapper from '@/components/wrapper'
+import PushInternal from '@/components/pushInternal'
  
 export default function Project() {
   const router = useRouter()
@@ -28,37 +29,50 @@ export default function Project() {
   return (
     <Wrapper>
     {project && 
-      <div className="mt-16 flex flex-col lg:grid lg:grid-cols-12 gap-8">
-        <div className="lg:col-span-5 lg:col-start-8">
-          <h1 className="text-6xl md:text-8xl font-bold mb-16">{project.title}</h1>
-          <div className="flex flex-row">
-            <div className="mr-12">
-              <p className="font-mono uppercase">Year</p>
-              <p className="text-xl md:text-2xl">{project.year}</p>
+          <>
+            <div className="mt-16 flex flex-col lg:grid lg:grid-cols-12 gap-8">
+              <div className="lg:col-span-5 lg:col-start-8">
+                <h1 className="text-6xl md:text-8xl font-bold mb-16">{project.title}</h1>
+                <div className="flex flex-row">
+                  <div className="mr-12">
+                    <p className="font-mono uppercase">Year</p>
+                    <p className="font-medium text-xl md:text-2xl">{project.year}</p>
+                  </div>
+                  <div>
+                    <p className="font-mono uppercase">Role</p>
+                    <p className="font-medium text-xl md:text-2xl">{project.role}</p>
+                  </div>
+                </div>
+              </div>
+              <div className="lg:col-span-6 lg:col-start-1 lg:row-start-2">
+                <div className="overflow-hidden rounded-l md:rounded-xl">
+                  <Image src={`/images/projects/${project.slug}.jpg`} alt={project.title} width="1200" height="900"/>
+                </div>
+              </div>
+              <div className="lg:col-span-5 lg:col-start-8 lg:row-start-2">
+                <p className="text-xl md:text-2xl mb-16">{project.longDescription}</p>
+                {project.link ? (
+                  <Button href={project.link} label="View Project" />
+                ) : null}
+              </div>
             </div>
-            <div>
-              <p className="font-mono uppercase">Role</p>
-              <p className="text-xl md:text-2xl">{project.role}</p>
-            </div>
+            <div className=" mt-16 md:mt-32 flex flex-row justify-between lg:grid lg:grid-cols-12 gap-8">
+              <div className="lg:col-span-4 lg:col-start-1">
+                {hasPrevious ? (
+                  <PushInternal href={`/projects/${previousProject.slug}`} label={previousProject.title} />
+                ) : (
+                  <PushInternal href={"/projects"} label="All projects" />
+                )}
+              </div>
+              <div className="lg:col-span-4 lg:col-start-9">
+                {hasNext ? (
+                  <PushInternal href={`/projects/${nextProject.slug}`} label={nextProject.title} />
+                ) : (
+                  <PushInternal href={"/projects"} label="All projects" />
+                )}
+              </div>
           </div>
-        </div>
-        <div className="lg:col-span-6 lg:col-start-1 lg:row-start-2">
-          <div className="overflow-hidden rounded-l md:rounded-xl">
-            <Image src={`/images/projects/${project.slug}.jpg`} alt={project.title} width="1200" height="900"/>
-          </div>
-        </div>
-        <div className="lg:col-span-5 lg:col-start-8 lg:row-start-2">
-          <p className="text-xl md:text-2xl mb-16">{project.longDescription}</p>
-          <div className="flex flex-row justify-between">
-            {hasPrevious && (
-              <ButtonInternal href={`/projects/${previousProject.slug}`} label={'Previous'} />
-            )}
-            {hasNext && (
-              <ButtonInternal href={`/projects/${nextProject.slug}`} label={'Next'} />
-            )}
-          </div>
-        </div>
-      </div>
+        </>
       }
     </Wrapper>
   )
