a0850e74a873907625c774a83f36585ff66c1426
Adrien <adrien.galibert@gmail.com>
2023-12-22 13:33:08 +0100

    Add initial hover transition for project list items

--- src/components/projectListItem.js
+++ src/components/projectListItem.js
@@ -2,13 +2,13 @@ import Link from "next/link"
 
 export default function ProjectListItem({ project, key }) {
     return (
-        <li key={project.slug}>
-                        <Link href={`/projects/${project.slug}`}>
-                            <div className="flex justify-between items-center pt-4 pb-4">
-                                <h2 className="text-2xl md:text-3xl font-medium">{project.title}</h2>
-                                <span className="text-2xl md:text-3xl">{project.year}</span>
-                            </div>
-                        </Link>
-                    </li>
-        )
+        <li key={project.slug} className="relative overflow-hidden">
+            <Link href={`/projects/${project.slug}`} className="group before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-pink before:translate-y-20 before:origin-top before:transition before:duration-200 hover:before:translate-y-0 hover:before:origin-bottom">
+                <div className="flex justify-between items-center pt-4 pb-4">
+                    <h2 className="group-hover:translate-x-4 transition duration-300 text-2xl md:text-3xl font-medium">{project.title}</h2>
+                    <span className="group-hover:-translate-x-4 transition duration-300 text-2xl md:text-3xl">{project.year}</span>
+                </div>
+            </Link>
+        </li>
+    )
 }
