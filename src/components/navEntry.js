6c9c9ba7b3020ea929864f34180a94771ba096b5
Adrien <adrien.galibert@gmail.com>
2023-12-24 19:24:09 +0100

    Add hover + active nav entry states

--- src/components/navEntry.js
+++ src/components/navEntry.js
@@ -1,11 +1,24 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
 
 export default function NavEntry({ href, children }) {
+    const pathname = usePathname();
+    const isActive = pathname === href;
+
     return (
-        <li className="ml-12 overflow-hidden h-6">
+        <li className={clsx("group ml-12 overflow-hidden h-6 before:content-['•'] before:absolute before:text-3xl before:-translate-y-[7px] before:transition duration-300", {
+            "before:opacity-100": isActive,
+            "before:-translate-x-6": isActive,
+            "before:opacity-0" : !isActive,
+            "before:-translate-x-4": !isActive,
+            "hover:before:opacity-100": !isActive,
+            "hover:before:-translate-x-6": !isActive,
+        }
+        )}>
             <div className="relative flex flex-col gap-0 translate-y-0 hover:-translate-y-6 transition duration-300">
-                <Link href={href} className="">{children}</Link>
-                <Link href={href} className="">{children}</Link>
+                <Link href={href}>{children}</Link>
+                <Link href={href}>{children}</Link>
             </div>
         </li>
     )
