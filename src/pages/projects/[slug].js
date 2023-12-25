b995e14ab2ad149af6a5cf9ff807dc36a3d97a13
Adrien <adrien.galibert@gmail.com>
2023-12-25 18:44:45 +0100

    Add basic page transition between projects [WIP]

--- src/pages/projects/[slug].js
+++ src/pages/projects/[slug].js
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import Wrapper from '@/components/wrapper'
 import PushDefault from '@/components/pushDefault'
 import PushProject from '@/components/pushProject'
+import { motion, AnimatePresence } from 'framer-motion'
  
 export default function Project() {
   const router = useRouter()
@@ -26,60 +27,79 @@ export default function Project() {
   const previousProject = hasPrevious ? sortedList[previousIndex] : null
   const nextProject = hasNext ? sortedList[nextIndex] : null
 
+  const transitionSpringPhysics = {
+    type: "spring",
+    mass: 0.2,
+    stiffness: 80,
+    damping: 10,
+  };
+
+  const transitionColor = '#181820';
+
   return (
-    <Wrapper>
-    {project && 
-          <>
-            <div className="mt-16 flex flex-col lg:grid lg:grid-cols-12 gap-8">
-              <div className="lg:col-span-5 lg:col-start-8">
-                <h1 className="text-6xl md:text-8xl font-bold mb-16">{project.title}</h1>
-                <div className="flex flex-row">
-                  <div className="mr-12">
-                    <p className="font-mono uppercase">Year</p>
-                    <p className="font-medium text-xl md:text-2xl">{project.year}</p>
-                  </div>
-                  <div>
-                    <p className="font-mono uppercase">Role</p>
-                    <p className="font-medium text-xl md:text-2xl">{project.role}</p>
-                  </div>
-                </div>
-              </div>
-              <div className="lg:col-span-6 lg:col-start-1 lg:row-start-2">
-                <div className="overflow-hidden rounded-l md:rounded-xl">
-                  <Image src={project.img} alt={project.title} width="1200" height="900"/>
-                </div>
-              </div>
-              <div className="lg:col-span-5 lg:col-start-8 lg:row-start-2">
-                <p className="text-xl md:text-2xl mb-16">{project.longDescription}</p>
-                {project.link ? (
-                  <Button href={project.link}>View live website</Button>
-                ) : null}
-              </div>
-            </div>
-            <div className="flex flex-col lg:flex-row gap-8 mt-16 md:mt-32">
-              {hasPrevious ? (
-                <div className="basis-1/2">
-                  <PushProject project={previousProject} format="small" />
-                </div>
-              ) : (
-                <div className="basis-1/2">
-                  <PushDefault href={"/projects"}>All projects</PushDefault>
-                </div>
-              )}
-              {hasNext ? ( 
-                    <div className="basis-1/2">
-                      <PushProject project={nextProject} format="small" />
+    project && (
+      <AnimatePresence
+        mode="wait"
+        initial={false}
+        onExitComplete={() => window.scrollTo(0, 0)}
+      >
+        <motion.div
+          key={project.slug}
+          initial={{ opacity: 0, y: 20  }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, transition: { duration: 0.5 } }}
+          transition={{ duration: 0.5, ease: "easeInOut" }}
+        >
+          <Wrapper>
+              <div className="mt-16 flex flex-col lg:grid lg:grid-cols-12 gap-8">
+                <div className="lg:col-span-5 lg:col-start-8">
+                  <h1 className="text-6xl md:text-8xl font-bold mb-16">{project.title}</h1>
+                  <div className="flex flex-row">
+                    <div className="mr-12">
+                      <p className="font-mono uppercase">Year</p>
+                      <p className="font-medium text-xl md:text-2xl">{project.year}</p>
                     </div>
-                  ) : (
-                    <div className="basis-1/2">
-                      <PushDefault href={"/projects"}>All projects</PushDefault>
+                    <div>
+                      <p className="font-mono uppercase">Role</p>
+                      <p className="font-medium text-xl md:text-2xl">{project.role}</p>
                     </div>
-              )}
-            </div>
-        </>
-      }
-    </Wrapper>
+                  </div>
+                </div>
+                <div className="lg:col-span-6 lg:col-start-1 lg:row-start-2">
+                  <div className="overflow-hidden rounded-l md:rounded-xl">
+                    <Image src={project.img} alt={project.title} width="1200" height="900"/>
+                  </div>
+                </div>
+                <div className="lg:col-span-5 lg:col-start-8 lg:row-start-2">
+                  <p className="text-xl md:text-2xl mb-16">{project.longDescription}</p>
+                  {project.link ? (
+                    <Button href={project.link}>View live website</Button>
+                  ) : null}
+                </div>
+              </div>
+              <div className="flex flex-col lg:flex-row gap-8 mt-16 md:mt-32">
+                {hasPrevious ? (
+                  <div className="basis-1/2">
+                    <PushProject project={previousProject} format="small" />
+                  </div>
+                ) : (
+                  <div className="basis-1/2">
+                    <PushDefault href={"/projects"}>All projects</PushDefault>
+                  </div>
+                )}
+                {hasNext ? ( 
+                      <div className="basis-1/2">
+                        <PushProject project={nextProject} format="small" />
+                      </div>
+                    ) : (
+                      <div className="basis-1/2">
+                        <PushDefault href={"/projects"}>All projects</PushDefault>
+                      </div>
+                )}
+              </div>
+          </Wrapper>
+        </motion.div>
+      </AnimatePresence>
+    )
   )
 }
-
-              
